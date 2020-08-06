f3d0a0bbed401775da46baa8f27c004257c642d5
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-08-06 19:11:48 -0300

    feature(utils): Utils page and main mustache improved
    Course image added

--- classes/extras/utils.php
+++ classes/extras/utils.php
@@ -4,6 +4,10 @@ namespace block_dashboard\extras;
 
 require_once("$CFG->dirroot/course/lib.php");
 
+use stdClass;
+use core_course_category;
+use moodle_url;
+
 /**
  * Useful functions class
  * 
@@ -23,13 +27,54 @@ class utils {
 
         foreach ($courses as $course) {
             $course = course_get_format($course->id)->get_course();
+            
             if ($course->isunit) {
                 continue;
             }
+
+            $course->courseimage = utils::get_course_summary_image($course);
+            $course->viewurl = new moodle_url("/course/view.php", array(
+                "id" => $course->id,
+            ));
 
             $response[] = $course;
         }
 
         return $response;
+    }
+
+    /**
+     *
+     */
+    public static function get_course_summary_image(stdClass $course) {
+        global $CFG;
+
+        $url = "$CFG->wwwroot/theme/saiteava/pix/theme/default-course.jpg";
+        $category = core_course_category::get($course->category);
+        $courses = $category->get_courses();
+
+        foreach ($courses as $categoryCourse) {
+            if ($categoryCourse->id === $course->id) {
+                foreach ($categoryCourse->get_course_overviewfiles() as $file) {
+                    if ($file->is_valid_image()) {
+                        $components = array(
+                            "/pluginfile.php",
+                            $file->get_contextid(),
+                            $file->get_component(),
+                            $file->get_filearea(),
+                            $file->get_filepath(),
+                            $file->get_filename(),
+                        );
+
+                        $path = implode("/", $components);
+                        return (new moodle_url($path))->out();
+                    }
+                }
+
+                break;
+            }
+        }
+
+        return $url;
     }
 }
